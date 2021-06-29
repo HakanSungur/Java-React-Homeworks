bbbb7c159c5608a0c6783428ee37aa455e605cda
HakanSungur <hakansungur@gmail.com>
2021-06-29 14:29:13 +0300

    29.06.2021 14.29

---
+++ src/week2/CourseManager.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class CourseManager {
+	public void getCourseDetails(Course course) {
+		System.out.println("Kurs Detayı"+ course.detail);
+	}
+	
+	public void getCourseTeacher(Course course) {
+		System.out.println("Kurs Öğrentmeni"+ course.teacherId);
+	}
+	
+
+}
